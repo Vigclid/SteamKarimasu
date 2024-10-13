dd829f82c372e70eeac8d35b6919708504e1d2e7
Đoàn Xuân Sơn <limbo04112004@gmail.com>
2024-10-14 00:09:05 +0700

    Small Update

--- src/java/controller/LoginServlet.java
+++ src/java/controller/LoginServlet.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
+
 package controller;
 
 import java.io.IOException;
@@ -34,10 +31,10 @@ public class LoginServlet extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet StudentServlet</title>");
+            out.println("<title>Final Project</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet StudentServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Login at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
@@ -73,6 +70,7 @@ public class LoginServlet extends HttpServlet {
             throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         response.setContentType("text/html;charset=UTF-8");
+        
         processRequest(request, response);
 
     }
